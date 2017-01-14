adebcfb94c4c91c8780d85a93436d249ebd89aeb
XuanAnh <xuananh1059@gmail.com>
2017-01-14 21:47:26 +0700

    up theme and plugin

---
+++ wp-content/themes/atopiclair/templates/about.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ Template Name: About
+ */
+ get_header(); ?>
+<div class="content">
+        <div class="about bg-cloud">
+            <div class="container-fluid">
+                <div class="info">
+                    <h2 class="info__title">
+                        atopiclai rtm <br> dưỡng ẩm, chống viêm, giảm ngứa
+                    </h2>
+                    <div class="info__title info__title--small">
+                        Đáp ứng tiêu chuẩn lựa chọn chất dưỡng ẩm điều trị vdcđ của Hội Da Liễu & Hội Nhi Khoa Việt Nam
+                    </div>
+                    <div class="info__thumb-intro">
+                        <?php printf('<img src="%s" alt="No Photo"/>',ATOPICLAIR_THEME_URL. '/images/about_img.png'); ?>
+                    </div>
+                    <div class="line-white info__product-guide">
+                        <h3 class="info__title line-white__title">cách sử dụng</h3>
+                        <div class="line-white__content">
+                            <div class="item">
+                                <p>
+                                    Thoa 2-3 lần/ngày. Khô da nhiều: tăng số lần sử dụng. Nên thoa lan rộng ra bên ngoài
+                                    vùng da tổn thương.
+                                </p>
+                            </div>
+                            <div class="item">
+                                <p>Thoa ngay sau khi tắm trong vòng 3-5 phút.</p>
+
+                            </div>
+                            <div class="item">
+                                <p>
+                                    Sử dụng phối hợp corticoid trong giai đoạn cấp (2 tuần đầu). Thoa dưỡng ẩm trước,
+                                    sau đó thoa corticoid.
+                                </p>
+                            </div>
+                            <div class="item">
+                                <p>
+                                    Thoa duy trì hàng ngày, dù không còn tổn thương nhằm dự phòng tái phát.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="thumb-block info__product-thumb">
+                        <div class="item">
+                            <div class="thumb-block__title">
+                                <h4 class="big">ATOPICLAIRTM Cream: </h4>
+                                <p class="small">
+                                    DÙNG ĐIỀU TRỊ TRIỆU CHỨNG, CÁC VÙNG TỔN THƯƠNG KHU TRÚ
+                                </p>
+                            </div>
+                            <div class="thumb-block__img">
+                                <?php printf('<img src="%s" alt="No Photo"/>',ATOPICLAIR_THEME_URL. '/images/about_product1.png'); ?>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="thumb-block__title">
+                                <h4 class="big">ATOPICLAIRTM Lotion: </h4>
+                                <p class="small">
+                                    DÙNG ĐIỀU TRỊ, DUY TRÌ, CÁC VÙNG TỔN THƯƠNG CÓ DIỆN TÍCH RỘNG, VÙNG CÓ LÔNG, TÓC
+                                </p>
+                            </div>
+                            <div class="thumb-block__img">
+                                <?php printf('<img src="%s" alt="No Photo"/>',ATOPICLAIR_THEME_URL. '/images/about_product2.png'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info__product-choose-us">
+                        <h2 class="info__title">vì sao nên sử dụng atopiclairtm?</h2>
+                        <div class="item">
+                            <p class="text-drs bold">
+                                Atopiclairtm hỗ trợ điều trị VDCĐ không chứa corticoid được FDA Hoa Kỳ chứng nhân
+                            </p>
+                        </div>
+                        <div class="item">
+                            <p class="text-drs">
+                                Atopiclairtm hơn hẳn các chất dưỡng ẩm thông thường vì có thêm hoạt tính chống viêm,
+                                giảm ngứa được Hội Da Liễu và Hội Nhi Khoa Việt Nam khuyến cáo ưu tiên lựa chọn
+                            </p>
+                            <p class="text-drs">
+                                <span>Giảm ngứa nhanh troCng vòng < 3 phút</span>
+                                <span>Hơn 90% bệnh nhân giảm liều lượng và thời gian sử dụng cortitoid trong kiểm soát VDCĐ</span>
+                            </p>
+                        </div>
+                        <div class="item">
+                            <p class="text-drs">AtopiclairTM – An toàn cho trẻ em và trẻ nhũ nhi</p>
+                        </div>
+                        <div class="item">
+                            <p class="text-drs">
+                                Nên sử dụng AtopiclairTM duy trì hàng ngày cho bệnh nhân VDCĐ, dù không còn tổn thương
+                                nhằm
+                                dự phòng tái phát.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
+
+
--- wp-content/themes/atopiclair/templates/homepage.php
+++ wp-content/themes/atopiclair/templates/homepage.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php
+/*
+ Template Name: Homepage
+ */
+get_header(); ?>
     <div class="content">
         <div class="home">
             <div class="blog blog--intro">
