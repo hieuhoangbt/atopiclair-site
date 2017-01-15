81b5b0d07d164c73b1a4381f648364cd63276356
Viet Ho <viet.ho@hdwebsoft.co>
2017-01-16 02:39:02 +0700

    add phan phoi and dist

--- app/js/start.js
+++ app/js/start.js
@@ -1,4 +1,10 @@
 window.onload = function () {
+    $('.scroll-pane').jScrollPane({
+        showArrows: true,
+        width: '100%',
+        height: 227
+    });
+
     $('.thumnail-reason .item').matchHeight({property: 'min-height', byRow: true});
     $(window).on("orientationchange", function () {
         $.fn.matchHeight._update();
@@ -12,5 +18,13 @@ window.onload = function () {
         offsetY: 15,
         blockElement: '.item'
     });
+    /*select*/
+    $('.select-province, .select-township').selectBox({
+        mobile: true,
+        menuTransition: 'default',
+        hideOnWindowScroll: true
+    });
+
 
 }
+
