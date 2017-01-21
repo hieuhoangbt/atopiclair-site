7049a557d66b93a9b580e9f269b1360b5e39d8d3
Hieu Hoang <trunghieu186@gmail.com>
2017-01-21 10:26:58 +0700

    php: Update home + single

---
+++ wp-content/themes/atopiclair/home.php
@@ -0,0 +1,173 @@
+<?php get_header(); ?>
+    <div class="content">
+        <div class="home">
+            <div class="blog blog--intro">
+                <div class="container-fluid">
+                    <div class="blog__ders">
+                        <h2 class="title-info title-info--big blog__ders__title">với Atopiclair<sup>TM</sup> <br> bé hết ngứa - Mẹ hết lo</h2>
+                        <div class="list-inline inline-link">
+                            <a class="btn-link inline-link__btn" href="<?php echo get_page_link(58); ?>">Bệnh Viêm Da Cơ Địa</a>
+                            <a class="btn-link inline-link__btn" href="<?php echo get_page_link(61); ?>">hướng dẫn điều trị</a>
+                            <a class="btn-link inline-link__btn" href="<?php echo get_page_link(9); ?>">Về Atopiclair</a>
+                            <a class="btn-link inline-link__btn" href="<?php echo get_page_link(66); ?>">Chia sẻ</a>
+                        </div>
+                    </div>
+                    <div class="blog__thumb">
+                        <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_gau.png" alt="Atopiclair">
+                    </div>
+                </div>
+            </div>
+            <div class="blog blog--nguyennhan">
+                <div class="container-fluid">
+                    <div class="blog__ders">
+                        <div class="quote-info blog__ders__quote">
+                            <h2 class="title-info title-info--big blog__ders__title">bệnh viêm da cơ địa</h2>
+                            <div class="box">
+                                <span class="quote-icon quote-icon--left">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-left.png" alt="bệnh viêm da cơ địa">
+                                </span>
+                                <p>
+                                    Còn gọi là chàm cơ địa, chàm thể tạng.
+
+                                </p>
+                                <p>
+                                    Là bệnh da mạn tính xen kẽ với những giai đoạn bùng phát và rất dễ tái phát. Bệnh
+                                    khó
+                                    điều trị dứt hẳn nhưng có thể phòng ngừa. Khoảng 50% trẻ em viêm da cơ địa vẫn còn
+                                    bệnh
+                                    khi đã trưởng thành.
+
+                                </p>
+                                <p>
+                                    Phổ biến ở trẻ em, chiếm 26.6% trẻ nhũ nhi và 16% ở trẻ dưới 5 tuổi.
+
+                                </p>
+                                <span class="quote-icon quote-icon--right">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-right.png" alt="bệnh viêm da cơ địa">
+                                </span>
+                            </div>
+                            <a href="<?php echo get_page_link(58); ?>" class="btn-link">Xem thêm</a>
+                        </div>
+                    </div>
+                    <div class="blog__thumb">
+                        <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_hand.png" alt="bệnh viêm da cơ địa">
+                    </div>
+                </div>
+            </div>
+            <div class="blog blog--dieutri">
+                <div class="container-fluid">
+                    <div class="blog__ders">
+                        <div class="quote-info blog__ders__quote">
+                            <h2 class="title-info title-info--big blog__ders__title">hướng dẫn điều trị</h2>
+                            <div class="box">
+                                <span class="quote-icon quote-icon--left">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-left.png" alt="Atopiclair hướng dẫn điều trị">
+                                </span>
+                                <p>
+                                    Bệnh VDCĐ khởi phát từ sự suy yếu hang rào bảo vệ da, dẫn đến da khô, ngứa và sự xâm
+                                    nhập của các dị nguyên và vi khuẩn, gây nhiễm trùng da. Do vậy, điều trị viêm da cơ
+                                    địa
+                                    bằng chất dưỡng ẩm da là một điều trị nền tảng trong các giai đoạn của bệnh.
+
+                                </p>
+                                <p>
+                                    Bác sĩ sẽ có thêm các điều trị khác tùy vào tình trạng của mỗi bệnh nhân như thay
+                                    đổi
+                                    lối sống, dùng thuốc để chống đỏ, sung (chống viêm), giảm ngứa (kháng histamine),
+                                    giảm
+                                    stress tâm lý và điều trị nhiễm trùng (nếu có) để ngăn ngừa hình thành tổn thương
+                                    mới.
+
+                                </p>
+                                <span class="quote-icon quote-icon--right">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-right.png" alt="Atopiclair hướng dẫn điều trị">
+                                </span>
+                            </div>
+                            <a href="<?php echo get_page_link(61); ?>" class="btn-link">Xem thêm</a>
+                        </div>
+                    </div>
+                    <div class="blog__thumb">
+                        <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_chan.png" alt="Atopiclair hướng dẫn điều trị">
+                    </div>
+                </div>
+            </div>
+            <div class="blog blog--about">
+                <div class="container-fluid">
+                    <div class="blog__ders">
+                        <div class="quote-info blog__ders__quote">
+                            <h2 class="title-info title-info--big blog__ders__title">Về Atopiclair<sup>TM</sup></h2>
+                            <div class="box">
+                                <span class="quote-icon quote-icon--left">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-left.png" alt="Về Atopiclair">
+                                </span>
+                                <p>
+                                    Tái tạo hàng rào bảo vệ da qua việc cung cấp các lipid thiết yếu và tăng mạnh khả
+                                    năng giữ nước cho da
+                                </p>
+                                <p>
+                                    Giảm ngứa, bảo vệ hàng rào da nhờ tác động chống oxy hóa mạnh, ức chế các enzyme,
+                                    protein bất lợi
+                                </p>
+                                <p>
+                                    Làm giảm cơn ngứa và rát da qua tác động chống ngứa và kháng viêm hiệu quả
+                                </p>
+                                <span class="quote-icon quote-icon--right">
+                                    <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_quote-right.png" alt="Về Atopiclair">
+                                </span>
+                            </div>
+                            <a href="<?php echo get_page_link(9); ?>" class="btn-link">Xem thêm</a>
+                        </div>
+                    </div>
+                    <div class="blog__thumb">
+                        <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/home_about.png" alt="Về Atopiclair">
+                    </div>
+                </div>
+            </div>
+            <div class="blog blog--share">
+                <div class="container-fluid">
+                    <div class="blog__ders">
+                        <h2 class="title-info title-info--big blog__ders__title">bé hết ngứa - Mẹ hết lo</h2>
+                        <a href="<?php echo get_page_link(66); ?>" class="btn-link">chia sẻ</a>
+                        <div class="quote-info blog__ders__quote">
+                            <ul class="list-inline list-children">
+                                <?php
+                                $post_params = array(
+                                    'posts_per_page'=>3,
+                                    'post_type' => 'story',
+                                    'post_status' => 'publish',
+                                    'highlight' =>1,
+                                    'orderby' => 'date',
+                                    'order' => 'DESC',
+                                );
+                                $liststory = new WP_Query($post_params);
+                                if ($liststory->have_posts()) {
+                                    while ($liststory->have_posts()) {
+                                        $liststory->the_post();
+                                        $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), "post_thumbnail");
+                                        ?>
+                                        <li class="item">
+                                            <div class="list-children__thumb">
+                                                <?php
+                                                if ($image) {?>
+                                                    <img src="<?php echo $image[0]; ?>"/>
+                                                <?php } else {
+                                                    printf('<img src="%s" alt="%s" />', ATOPICLAIR_THEME_URL . '/images/placeholder.png', the_title());
+                                                }
+                                                ?>
+                                                <img src="<?php ATOPICLAIR_THEME_URL ?>/images/home_be-anh.png" alt="<?php the_title(); ?>">
+                                            </div>
+                                            <div class="list-children__name"><?php the_title(); ?></div>
+                                        </li>
+                                    <?php }
+                                    wp_reset_query();
+                                } ?>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
