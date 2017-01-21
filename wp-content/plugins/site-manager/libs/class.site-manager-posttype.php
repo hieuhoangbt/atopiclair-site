06f40601ced18304e19ee467440f7754860bc287
XuanAnh <xuananh1059@gmail.com>
2017-01-21 10:52:43 +0700

    update

--- wp-content/plugins/site-manager/libs/class.site-manager-posttype.php
+++ wp-content/plugins/site-manager/libs/class.site-manager-posttype.php
@@ -238,7 +238,8 @@ class SiteManagerPosttype {
             'capability_type' => 'page',
         );
         register_post_type('expert', $args);
-
+        
+        //Doctor
         $labels = array(
             'name' => _x('Doctor', 'Post Type General Name', 'vuonxa'),
             'singular_name' => _x('Doctor', 'Post Type Singular Name', 'vuonxa'),
@@ -295,119 +296,6 @@ class SiteManagerPosttype {
 
         register_post_type('doctor', $args);
 
-        $labels = array(
-            'name' => _x('Doctor post', 'Post Type General Name', 'vuonxa'),
-            'singular_name' => _x('Doctor post', 'Post Type Singular Name', 'vuonxa'),
-            'menu_name' => __('Doctor posts', 'vuonxa'),
-            'name_admin_bar' => __('Doctor post', 'vuonxa'),
-            'archives' => __('Item Archives', 'vuonxa'),
-            'parent_item_colon' => __('Parent Item:', 'vuonxa'),
-            'all_items' => __('All Items', 'vuonxa'),
-            'add_new_item' => __('Add New Item', 'vuonxa'),
-            'add_new' => __('Add New', 'vuonxa'),
-            'new_item' => __('New Item', 'vuonxa'),
-            'edit_item' => __('Edit Item', 'vuonxa'),
-            'update_item' => __('Update Item', 'vuonxa'),
-            'view_item' => __('View Item', 'vuonxa'),
-            'search_items' => __('Search Item', 'vuonxa'),
-            'not_found' => __('Not found', 'vuonxa'),
-            'not_found_in_trash' => __('Not found in Trash', 'vuonxa'),
-            'featured_image' => __('Featured Image', 'vuonxa'),
-            'set_featured_image' => __('Set featured image', 'vuonxa'),
-            'remove_featured_image' => __('Remove featured image', 'vuonxa'),
-            'use_featured_image' => __('Use as featured image', 'vuonxa'),
-            'insert_into_item' => __('Insert into item', 'vuonxa'),
-            'uploaded_to_this_item' => __('Uploaded to this item', 'vuonxa'),
-            'items_list' => __('Items list', 'vuonxa'),
-            'items_list_navigation' => __('Items list navigation', 'vuonxa'),
-            'filter_items_list' => __('Filter items list', 'vuonxa'),
-        );
-        $rewrite = array(
-            'slug' => 'doctorpost',
-            'with_front' => true,
-            'pages' => true,
-            'feeds' => true,
-        );
-        $args = array(
-            'label' => __('Doctor post', 'vuonxa'),
-            'description' => __('Doctor post', 'vuonxa'),
-            'labels' => $labels,
-            'supports' => array('title', 'editor', 'slug'),
-            'hierarchical' => false,
-            'public' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'menu_position' => 5,
-            'menu_icon' => 'dashicons-welcome-write-blog',
-            'show_in_admin_bar' => true,
-            'show_in_nav_menus' => true,
-            'can_export' => true,
-            'has_archive' => true,
-            'exclude_from_search' => false,
-            'publicly_queryable' => true,
-            'rewrite' => $rewrite,
-            'capability_type' => 'page',
-        );
-
-        register_post_type('doctorpost', $args);
-
-        //Video
-        $labels = array(
-            'name' => _x('Video', 'Post Type General Name', 'vuonxa'),
-            'singular_name' => _x('Video', 'Post Type Singular Name', 'vuonxa'),
-            'menu_name' => __('Videos', 'vuonxa'),
-            'name_admin_bar' => __('Video', 'vuonxa'),
-            'archives' => __('Item Archives', 'vuonxa'),
-            'parent_item_colon' => __('Parent Item:', 'vuonxa'),
-            'all_items' => __('All Items', 'vuonxa'),
-            'add_new_item' => __('Add New Item', 'vuonxa'),
-            'add_new' => __('Add New', 'vuonxa'),
-            'new_item' => __('New Item', 'vuonxa'),
-            'edit_item' => __('Edit Item', 'vuonxa'),
-            'update_item' => __('Update Item', 'vuonxa'),
-            'view_item' => __('View Item', 'vuonxa'),
-            'search_items' => __('Search Item', 'vuonxa'),
-            'not_found' => __('Not found', 'vuonxa'),
-            'not_found_in_trash' => __('Not found in Trash', 'vuonxa'),
-            'featured_image' => __('Featured Image', 'vuonxa'),
-            'set_featured_image' => __('Set featured image', 'vuonxa'),
-            'remove_featured_image' => __('Remove featured image', 'vuonxa'),
-            'use_featured_image' => __('Use as featured image', 'vuonxa'),
-            'insert_into_item' => __('Insert into item', 'vuonxa'),
-            'uploaded_to_this_item' => __('Uploaded to this item', 'vuonxa'),
-            'items_list' => __('Items list', 'vuonxa'),
-            'items_list_navigation' => __('Items list navigation', 'vuonxa'),
-            'filter_items_list' => __('Filter items list', 'vuonxa'),
-        );
-        $rewrite = array(
-            'slug' => 'Video',
-            'with_front' => true,
-            'pages' => true,
-            'feeds' => true,
-        );
-        $args = array(
-            'label' => __('Video', 'vuonxa'),
-            'description' => __('Video', 'vuonxa'),
-            'labels' => $labels,
-            'supports' => array('editor'),
-            'hierarchical' => false,
-            'public' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'menu_position' => 5,
-            'menu_icon' => 'dashicons-video-alt3',
-            'show_in_admin_bar' => true,
-            'show_in_nav_menus' => true,
-            'can_export' => true,
-            'has_archive' => true,
-            'exclude_from_search' => false,
-            'publicly_queryable' => true,
-            'rewrite' => $rewrite,
-            'capability_type' => 'page',
-        );
-
-        register_post_type('video', $args);
-
         flush_rewrite_rules();
         return $instance;
     }
@@ -426,10 +314,10 @@ class SiteManagerPosttype {
             $data = get_post_meta($post->ID, 'medicaladvice')[0];
             require_once PLG_PLUGIN_DIR . '/views/backend/medicaladvice.php';
         }, 'medicaladvice', 'normal', 'core');
-        add_meta_box('doctorpost', __('Custom Info', 'vuonxa'), function($post) {
-            $data = get_post_meta($post->ID, 'doctorpost')[0];
-            require_once PLG_PLUGIN_DIR . '/views/backend/doctorpost.php';
-        }, 'doctorpost', 'normal', 'core');
+        add_meta_box('doctor', __('Custom Info', 'vuonxa'), function($post) {
+            $data = get_post_meta($post->ID, 'doctor')[0];
+            require_once PLG_PLUGIN_DIR . '/views/backend/doctor.php';
+        }, 'doctor', 'normal', 'core');
 
         return $instance;
     }
@@ -470,24 +358,6 @@ class SiteManagerPosttype {
             'title' => $columns['title'],
             'content' => 'Content',
             'post_thumb' => 'Thumbnail',
-            'date' => $columns['date'],
-        );
-        return $columns;
-    }
-    public static function register_columns_doctorpost($columns) {
-        $columns = array(
-            'cb' => $columns['cb'],
-            'doctor' => 'Doctor',
-            'title' => $columns['title'],
-            'content' => 'Content',
-            'date' => $columns['date'],
-        );
-        return $columns;
-    }
-    public static function register_columns_video($columns) {
-        $columns = array(
-            'cb' => $columns['cb'],
-            'video' => 'Video',
             'date' => $columns['date'],
         );
         return $columns;
@@ -566,22 +436,6 @@ class SiteManagerPosttype {
                 break;
         }
     }
-    public static function list_doctorpost($columns, $post_id) {
-        $data = get_post_meta($post_id, 'doctorpost')[0];
-        $args = array(
-            'ID' => $data['doctor'],
-            'post_type'   => 'doctor'
-        );
-        $data_doctor=get_posts($args);
-        switch ($columns) {
-            case 'doctor':
-                echo $data_doctor[0]->post_title;
-                break;
-            case 'content':
-                echo get_the_content($post_id,'content');
-                break;
-        }
-    }
     public static function list_video($columns, $post_id) {
         $data = get_post_meta($post_id, 'video')[0];
         switch ($columns) {
@@ -622,10 +476,6 @@ class SiteManagerPosttype {
             case "medicaladvice":
                 $data = $_POST['medicaladvice'];
                 update_post_meta($post_id, 'medicaladvice', $data);
-                break;
-            case "doctorpost":
-                $data = $_POST['doctorpost'];
-                update_post_meta($post_id, 'doctorpost', $data);
                 break;
         }
     }
