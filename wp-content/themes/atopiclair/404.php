c99b09c4e049196368a626f6631aac37ad918c3b
Hieu Hoang <trunghieu186@gmail.com>
2017-01-22 21:45:31 +0700

    php: update page 404

--- wp-content/themes/atopiclair/404.php
+++ wp-content/themes/atopiclair/404.php
@@ -1,1 +1,28 @@
-404.php
+<?php get_header(); ?>
+<?php
+global $wp;
+$current_url = home_url(add_query_arg(array(),$wp->request));
+$subject = urlencode(sprintf("URL: %s không tìm thấy!", $subject));
+?>
+<div class="content">
+        <div class="lienhe">
+            <div class="lienhe__bg">
+                <div class="info">
+                    <div class="container-fluid">
+                        <h2 class="info__title">404 Not Found!</h2>
+                        <div class="info__lienhe">
+                            <div class="info__lienhe__left">
+                                <p>Đường dẫn bạn nhập không tồn tại!</p>
+                                <p>Chúng tôi rất tiếc về sự cố này. Để có thể được hỗ trợ nhanh nhất, bạn vui lòng liên hệ email: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=admin@atopiclair.com.vn&subject=<?php echo $subject; ?>" target="_blank">admin@atopiclair.com.vn</a></p>
+                                <p>Xin cảm ơn!</p>
+                            </div>
+                            <div class="img-lienhe" style="bottom: 0; right: 0; left: auto;">
+                                <img src="<?php echo ATOPICLAIR_THEME_URL; ?>/images/gau_lienhe.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
